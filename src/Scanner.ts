e2e779a10d587478fc80ef8ca8b2be18a97fcae9
Rasmus Storjohann <rasmus@storbot.ca>
2016-09-03 14:44:30 -0700

    Can scan filenames with deeper paths and without number prefix

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -2,14 +2,16 @@ export class Scanner
 {
     public scan(pathToMusicFile: string)
     {
-        var match = /\/([^\/]+)\/([^\/]+)\/(\d+[^\/]+)\.mp3/.exec(pathToMusicFile);
+        var match = /\/([^\/]+)\/([^\/]+)\/((\d+)?[^\/]+)\.mp3/.exec(pathToMusicFile);
         var artist = match[1];
         var album = match[2];
         var track = match[3];
+        var trackNumber = match[4] && parseInt(match[4]);
         return {
             artist: artist,
             album: album,
-            track: track
+            track: track,
+            trackNumber: trackNumber
         };
     }
 };
--- test/ScannerTests.ts
+++ test/ScannerTests.ts
@@ -3,25 +3,65 @@
 import * as chai from "chai";
 import { Scanner } from "../src/Scanner";
 
+var _theScanner : Scanner;
+beforeEach(() =>
+{
+    _theScanner = new Scanner();
+});
+
 describe("Scanner", () => {
-    var validPath = "root/artist/album/01 track.mp3";
+    describe("with valid path", () => {
+        var _scanned;
+        beforeEach(() =>
+        {
+            var validPath = "root/artist/album/01 track.mp3";
+            _scanned = _theScanner.scan(validPath);
+        });
 
-    var _scanned;
-    beforeEach(() =>
-    {
-        var scanner = new Scanner();
-        _scanned = scanner.scan(validPath);
+        it("Gets the artist name from path", () => {
+            chai.expect(_scanned.artist).equals("artist");
+        });
+
+        it("Gets the album name from path", () => {
+            chai.expect(_scanned.album).equals("album");
+        });
+
+        it("Gets the track name from path", () => {
+            chai.expect(_scanned.track).equals("01 track");
+        });
+
+        it("Gets the track number from path", () => {
+            chai.expect(_scanned.trackNumber).equals(1);
+        });
     });
 
-    it("Gets the artist name from path", () => {
-        chai.expect(_scanned.artist).equals("artist");
+    describe("with deep path", () => {
+        var _scanned;
+        beforeEach(() =>
+        {
+            var deepPath = "the/deep/path/artist/album/01 track.mp3";
+            _scanned = _theScanner.scan(deepPath);
+        });
+
+        it("looks at three last path elements", () => {
+            chai.expect(_scanned.artist).equals("artist");
+            chai.expect(_scanned.album).equals("album");
+            chai.expect(_scanned.track).equals("01 track");
+        });
     });
 
-    it("Gets the album name from path", () => {
-        chai.expect(_scanned.album).equals("album");
-    });
-
-    it("Gets the track name from path", () => {
-        chai.expect(_scanned.track).equals("01 track");
+    describe("with track missing numeric prefix", () => {
+        var _scanned;
+        beforeEach(() =>
+        {
+            var pathWithoutNumericPrefix = "the/deep/path/artist/album/track.mp3";
+            _scanned = _theScanner.scan(pathWithoutNumericPrefix);
+        });
+        it("gets the track name from path", () => {
+            chai.expect(_scanned.track).equals("track");
+        });
+        it("track number is null", () => {
+            chai.expect(_scanned.trackNumber).equals(undefined);
+        });
     });
 });
