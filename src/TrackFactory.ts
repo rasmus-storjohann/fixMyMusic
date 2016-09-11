2eb6febcdb14041ee16d02fc43ab473d8ec65062
Rasmus Storjohann <rasmus@storbot.ca>
2016-09-11 12:02:03 -0700

    Simplified track factory

--- src/TrackFactory.ts
+++ src/TrackFactory.ts
@@ -11,27 +11,19 @@ export class TrackFactory
         });
         return result;
     }
-    public createTrack(pathToMusicFile: string) : Track
+    public createTrack(path: string) : Track
     {
-        var elements = pathToMusicFile.split("/");
+        var elements = path.split("/");
         var count = elements.length;
         if (count < 3)
         {
-            throw new Error(pathToMusicFile + ": Invalid path to music file");
-        }
-        var artist = elements[count - 3];
-        var album = elements[count - 2];
-        var title = elements[count - 1];
-        var match = /((\d+)?[^\/]+\.mp3)/.exec(title);
-        if (!match)
-        {
-            throw new Error(pathToMusicFile + ": Invalid music file name");
+            throw new Error(path + ": Invalid path to music file");
         }
         return {
-            path: pathToMusicFile,
-            artist: artist,
-            album: album,
-            title: title
+            path: path,
+            artist: elements[count - 3],
+            album: elements[count - 2],
+            title: elements[count - 1]
         };
     }
 };
