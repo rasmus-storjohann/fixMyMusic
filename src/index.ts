0ce9acaf98b4cd34526914bb801bf3266e3e412f
Rasmus Storjohann <rasmus@storbot.ca>
2016-10-15 12:34:37 -0700

    Using npmlog for logging output

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@
 
 import { Application } from "../src/Application";
 import * as process from "process";
+import * as log from "npmlog";
 
-Application.main(process.argv, console);
+Application.main(process.argv, log);
