96d7ed592fdf798df9b61e139d6fdf86c31721c3
Rasmus Storjohann <rasmus@storbot.ca>
2017-02-25 11:39:15 -0800

    Stab at system for storing rules in json

---
+++ src/Rule.ts
@@ -0,0 +1,102 @@
+export interface Rule {
+        firstTrackNumber?: number, fixTrackName?: string, fixAlbumTitle?: AlbumFormat
+}
+
+export interface AlbumFormat {
+        form: string, instrument?: string, num?: number, opus?: number,
+            opus_number?: number, subTitle?: string, performer?: string,
+            mode?: string, key?: string
+}
+
+export class RuleFactory
+{
+        public parseAlbumRule(json: string): Rule
+        {
+                var parsed = JSON.parse(json);
+                return this.buildAlbumRule(parsed);
+        }
+        public buildAlbumRule(from): Rule
+        {
+                var to: Rule = {};
+
+                this.parseOptionalNumber(from, to, "firstTrackNumber");
+                this.parseOptionalString(from, to, "fixTrackName");
+
+                if (from.fixAlbumTitle)
+                {
+                        to.fixAlbumTitle = this.buildFormat(from.fixAlbumTitle);
+                }
+                return to;
+        }
+        private parseOptionalString(from, to, field: string, toField?: string)
+        {
+                if (!from[field]) return;
+
+                var fieldToAssignTo = toField || field;
+                to[fieldToAssignTo] = from[field] + "";
+        }
+        private parseOptionalNumber(from, to, field)
+        {
+                if (!from[field]) return;
+
+                to[field] = from[field] + 0;
+        }
+        public parseFormat(json: string): AlbumFormat
+        {
+                var from = JSON.parse(json);
+                return this.buildFormat(from);
+        }
+        public buildFormat(from) : AlbumFormat
+        {
+                var to: AlbumFormat = {form : from.form + ""};
+
+                this.parseOptionalString(from, to, "for", "instrument");
+                this.parseOptionalString(from, to, "subTitle");
+                this.parseOptionalString(from, to, "by", "performer");
+                this.parseOptionalNumber(from, to, "num");
+                this.parseOpus(from, to);
+                this.parseKeyAndMode(from, to);
+
+                return to;
+        }
+        private parseOpus(from, to)
+        {
+                if (!from.opus) return;
+
+                if (Array.isArray(from.opus))
+                {
+                        this.validateLengthIsTwo(from.opus);
+                        to.opus = from.opus[0] + 0;
+                        to.opus_number = from.opus[1] + 0;
+                }
+                else
+                {
+                        to.opus = from.opus + 0;
+                }
+        }
+        private validateLengthIsTwo(item: Array<string>)
+        {
+                if (item.length !== 2)
+                {
+                        throw new Error(
+                            "invalid opus array, should have two elements");
+                }
+        }
+        private parseKeyAndMode(from, to)
+        {
+                if (from.major && from.minor)
+                {
+                        throw new Error("major and minor keys given");
+                }
+                if (from.major)
+                {
+                        to.mode = "major";
+                        to.key = from.major + "";
+                }
+                if (from.minor)
+                {
+                        to.mode = "minor";
+                        to.key = from.minor + "";
+                }
+        }
+}
---
+++ test/RulesTests.ts
@@ -0,0 +1,109 @@
+import {expect} from "chai";
+import {beforeEach, describe, it} from "mocha";
+import {Rule, RuleFactory} from "../src/Rule";
+
+describe("Rule parser", () => {
+        it("can parse firstTrackNumber", () => {
+                var dto = { firstTrackNumber: 4 };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseAlbumRule(json);
+
+                expect(parsed).to.deep.equal(dto);
+        });
+        it("can parse fixTrackName", () => {
+                var dto = { fixTrackName: "foo" };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseAlbumRule(json);
+
+                expect(parsed).to.deep.equal(dto);
+        });
+        it("can parse fixAlbumTitle", () => {
+                var dto = { fixAlbumTitle: { form: "concerto" } };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseAlbumRule(json);
+
+                expect(parsed).to.deep.equal(dto);
+        });
+});
+
+describe("Album format parser", () => {
+        it("can parse form", () => {
+                var dto = { form: "concerto" };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseFormat(json);
+
+                expect(parsed).to.deep.equal(dto);
+        });
+        it("can parse instrument", () => {
+                var dto = { form: "concerto", for: "piano" };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseFormat(json);
+
+                expect(parsed).to.deep.equal({ form: "concerto", instrument: "piano" });
+        });
+        it("can parse subTitle", () => {
+                var dto = { form: "concerto", subTitle: "Eroica" };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseFormat(json);
+
+                expect(parsed).to.deep.equal({ form: "concerto", subTitle: "Eroica" });
+        });
+        it("can parse performer", () => {
+                var dto = { form: "concerto", by: "Brendel" };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseFormat(json);
+
+                expect(parsed).to.deep.equal({ form: "concerto", performer: "Brendel" });
+        });
+        it("can parse number", () => {
+                var dto = { form: "concerto", num: 2 };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseFormat(json);
+
+                expect(parsed).to.deep.equal({ form: "concerto", num: 2 });
+        });
+        it("can parse opus number", () => {
+                var dto = { form: "concerto", opus: 2 };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseFormat(json);
+
+                expect(parsed).to.deep.equal({ form: "concerto", opus: 2 });
+        });
+        it("can parse opus number and number within the opus", () => {
+                var dto = { form: "concerto", opus: [2, 4] };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseFormat(json);
+
+                expect(parsed).to.deep.equal({ form: "concerto", opus: 2, opus_number: 4 });
+        });
+        it("throws on opus number array having more than two elements", () => {
+                var dto = { form: "concerto", opus: [1, 2, 4] };
+                var json = JSON.stringify(dto);
+
+                expect(() => {new RuleFactory().parseFormat(json)}).to.throw(
+                                            Error,
+                                            /invalid opus array, should have two elements/);
+        });
+        it("can parse major key", () => {
+                var dto = { form: "concerto", major: "A" };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseFormat(json);
+
+                expect(parsed).to.deep.equal({ form: "concerto", mode: "major", key: "A" });
+        });
+        it("can parse minor key", () => {
+                var dto = { form: "concerto", minor: "A" };
+                var json = JSON.stringify(dto);
+                var parsed = new RuleFactory().parseFormat(json);
+
+                expect(parsed).to.deep.equal({ form: "concerto", mode: "minor", key: "A" });
+        });
+        it("throws if both minor and major keys are given", () => {
+                var dto = { form: "concerto", major: "A", minor: "A" };
+                var json = JSON.stringify(dto);
+
+                expect(() => {new RuleFactory().parseFormat(json)}).to.throw(
+                                            Error,
+                                            /major and minor keys given/);
+        });
+});
