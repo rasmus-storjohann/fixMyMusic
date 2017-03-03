0c77c5397e10082bd6a39ae7c6291c5fc6384fa8
Rasmus Storjohann <rasmus@storbot.ca>
2017-03-02 21:36:47 -0800

    Autogenerated json fixer specs

--- src/RulesFactory.ts
+++ src/RulesFactory.ts
@@ -18,21 +18,74 @@ export class RulesFactory
         public create()
         {
                 var allTheRules = Others.rules;
-
-                var json = JSON.stringify(allTheRules);
-                fs.writeFileSync(
-                    "/home/rasmus/Music/bin/src/fixers/Others.json", json);
+                this.dumpJson(Others.rules,
+                              "/home/rasmus/Music/bin/src/fixers/Others.json");
 
                 allTheRules["JS Bach"] = BachJs.rules;
+                this.dumpJson(BachJs.rules,
+                              "/home/rasmus/Music/bin/src/fixers/BachJs.json");
+
                 allTheRules["Beethoven"] = Beethoven.rules;
+                this.dumpJson(
+                    Beethoven.rules,
+                    "/home/rasmus/Music/bin/src/fixers/Beethoven.json");
+
                 allTheRules["Handel"] = Handel.rules;
+                this.dumpJson(Handel.rules,
+                              "/home/rasmus/Music/bin/src/fixers/Handel.json");
+
                 allTheRules["Haydn"] = Haydn.rules;
+                this.dumpJson(Haydn.rules,
+                              "/home/rasmus/Music/bin/src/fixers/Haydn.json");
+
                 allTheRules["Mahler"] = Mahler.rules;
+                this.dumpJson(Mahler.rules,
+                              "/home/rasmus/Music/bin/src/fixers/Mahler.json");
+
                 allTheRules["Mozart"] = Mozart.rules;
+                this.dumpJson(Mozart.rules,
+                              "/home/rasmus/Music/bin/src/fixers/Mozart.json");
+
                 allTheRules["Frédéric Chopin"] = Copin.rules;
+                this.dumpJson(Copin.rules,
+                              "/home/rasmus/Music/bin/src/fixers/Copin.json");
+
                 allTheRules["Schubert"] = Schubert.rules;
+                this.dumpJson(
+                    Schubert.rules,
+                    "/home/rasmus/Music/bin/src/fixers/Schubert.json");
+
                 allTheRules["Shostakovich"] = Shostakovich.rules;
+                this.dumpJson(
+                    Shostakovich.rules,
+                    "/home/rasmus/Music/bin/src/fixers/Shostakovich.json");
 
                 return allTheRules;
         };
+        private dumpJson(data, path)
+        {
+                this.traverse(data, this.processObject);
+                var json = JSON.stringify(data);
+                fs.writeFileSync(path, json);
+        }
+        private processObject(key, data)
+        {
+                if (data[key] instanceof RegExp)
+                {
+                        var s = data[key].toString();
+                        data[key] = s.slice(1, s.length - 1);
+                }
+        }
+        private traverse(data, func)
+        {
+                for (var field in data)
+                {
+                        func.apply(this, [ field, data ]);
+                        if (data[field] !== null &&
+                            typeof(data[field]) == "object")
+                        {
+                                this.traverse(data[field], func);
+                        }
+                }
+        }
 };
